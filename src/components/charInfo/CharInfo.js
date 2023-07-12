8e9248f1feb5c9b8323f52125fa6763b50868786
beforedisappear <roman23kl@gmail.com>
2023-07-12 16:26:13 +0300

    class -> hooks

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,90 +1,64 @@
-import { Component } from "react";
-
 import Service from "../../services/service";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
 
+import { useState, useEffect } from "react";
+
 import "./charInfo.scss";
 
-class CharInfo extends Component {
-  state = {
-    char: null,
-    loading: false,
-    error: false,
-  };
+const CharInfo = (props) => {
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  marvelService = new Service();
+  const marvelService = new Service();
 
-  // первичный запрос для построения компонента
-  componentDidMount() {
-    this.updateChar();
-  }
+  useEffect(() => {
+    updateChar();
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.charId !== prevProps.charId) {
-      this.updateChar();
-    }
-  }
+  useEffect(() => {
+    updateChar();
+  }, [props.charId]);
 
   // метода для обновления компонента
-  updateChar = () => {
-    const { charId } = this.props;
-    if (!charId) {
-      return;
-    }
+  const updateChar = () => {
+    const { charId } = props;
+    if (!charId) return;
 
-    this.onCharLoading();
-
-    this.marvelService
-      .getCharacter(charId)
-      .then(this.onCharLoaded)
-      .catch(this.onError);
+    setLoading(() => true);
+    marvelService.getCharacter(charId).then(onCharLoaded).catch(onError);
   };
 
-
-  onCharLoaded = (char) => {
-    this.setState({
-      char,
-      loading: false,
-    });
+  const onCharLoaded = (char) => {
+    setLoading(false);
+    setChar(() => char);
   };
 
-  onCharLoading = () => {
-    this.setState({
-      loading: true,
-    });
+  const onError = () => {
+    setError(true);
+    setLoading(false);
   };
 
-  onError = () => {
-    this.setState({
-      loading: false,
-      error: true,
-    });
-  };
+  //начальное состояние (не загружен персонаж / не загрузка)
+  const skeleton = char || loading || error ? null : <Skeleton />;
+  //состояние если ошибка
+  const errorMessage = error ? <ErrorMessage /> : null;
+  //состояние загрузки
+  const spinner = loading ? <Spinner /> : null;
+  //отображение контента
+  const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-  render() {
-    const { char, loading, error } = this.state;
-
-    //начальное состояние (не загружен персонаж / не загрузка)
-    const skeleton = char || loading || error ? null : <Skeleton />;
-    //состояние если ошибка
-    const errorMessage = error ? <ErrorMessage /> : null;
-    //состояние загрузки
-    const spinner = loading ? <Spinner /> : null;
-    //отображение контента
-    const content = !(loading || error || !char) ? <View char={char} /> : null;
-
-    return (
-      <div className="char__info">
-        {skeleton}
-        {errorMessage}
-        {spinner}
-        {content}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="char__info">
+      {skeleton}
+      {errorMessage}
+      {spinner}
+      {content}
+    </div>
+  );
+};
 
 const View = ({ char }) => {
   const { name, description, preview, homepage, wiki, comics } = char;
@@ -116,7 +90,6 @@ const View = ({ char }) => {
       <div className="char__descr">{description}</div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
-
         {comics.length > 0 ? null : "There is no comics with this character"}
         {comics.map((item, i) => {
           // eslint-disable-next-line
@@ -133,3 +106,132 @@ const View = ({ char }) => {
 };
 
 export default CharInfo;
+
+//import { Component } from "react";
+
+//
+// class CharInfo extends Component {
+//   state = {
+//     char: null,
+//     loading: false,
+//     error: false,
+//   };
+
+//   marvelService = new Service();
+
+//   // первичный запрос для построения компонента
+//   componentDidMount() {
+//     this.updateChar();
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (this.props.charId !== prevProps.charId) {
+//       this.updateChar();
+//     }
+//   }
+
+//   // метода для обновления компонента
+//   updateChar = () => {
+//     const { charId } = this.props;
+//     if (!charId) {
+//       return;
+//     }
+
+//     this.onCharLoading();
+
+//     this.marvelService
+//       .getCharacter(charId)
+//       .then(this.onCharLoaded)
+//       .catch(this.onError);
+//   };
+
+//   onCharLoaded = (char) => {
+//     this.setState({
+//       char,
+//       loading: false,
+//     });
+//   };
+
+//   onCharLoading = () => {
+//     this.setState({
+//       loading: true,
+//     });
+//   };
+
+//   onError = () => {
+//     this.setState({
+//       loading: false,
+//       error: true,
+//     });
+//   };
+
+//   render() {
+//     const { char, loading, error } = this.state;
+
+//     //начальное состояние (не загружен персонаж / не загрузка)
+//     const skeleton = char || loading || error ? null : <Skeleton />;
+//     //состояние если ошибка
+//     const errorMessage = error ? <ErrorMessage /> : null;
+//     //состояние загрузки
+//     const spinner = loading ? <Spinner /> : null;
+//     //отображение контента
+//     const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+//     return (
+//       <div className="char__info">
+//         {skeleton}
+//         {errorMessage}
+//         {spinner}
+//         {content}
+//       </div>
+//     );
+//   }
+// }
+
+// const View = ({ char }) => {
+//   const { name, description, preview, homepage, wiki, comics } = char;
+
+//   let imgStyle = { objectFit: "cover" };
+//   if (
+//     preview ===
+//     "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+//   ) {
+//     imgStyle = { objectFit: "contain" };
+//   }
+
+//   return (
+//     <>
+//       <div className="char__basics">
+//         <img src={preview} alt={name} style={imgStyle} />
+//         <div>
+//           <div className="char__info-name">{name}</div>
+//           <div className="char__btns">
+//             <a href={homepage} className="button button__main">
+//               <div className="inner">homepage</div>
+//             </a>
+//             <a href={wiki} className="button button__secondary">
+//               <div className="inner">Wiki</div>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="char__descr">{description}</div>
+//       <div className="char__comics">Comics:</div>
+//       <ul className="char__comics-list">
+
+//         {comics.length > 0 ? null : "There is no comics with this character"}
+//         {comics.map((item, i) => {
+//           // eslint-disable-next-line
+//           if (i > 9) return;
+//           return (
+//             <li key={i} className="char__comics-item">
+//               {item.name}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default CharInfo;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,95 +3,69 @@ import mjolnir from "../../resources/img/mjolnir.png";
 import Service from "../../services/service";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
-class RandomChar extends Component {
-  constructor(props) {
-    super(props);
-  }
+const RandomChar = (props) => {
+  const [char, setChar] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  // пустой объект char эквивалентен записи в null
-  state = {
-    char: {},
-    loading: true,
-    error: false,
+  const marvelService = new Service();
+
+  useEffect(() => {
+    updateChar();
+    // const timedId = setInterval(updateChar, 10000);
+    // return () => clearInterval(timedId);
+  }, []);
+
+  const updateChar = () => {
+    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    charLoading();
+    marvelService.getCharacter(id).then(onCharLoaded).catch(onError); // вызов метода для обработки ошибки
   };
 
-  // state = {
-  //   name: null,
-  //   description: null,
-  //   preview: null,
-  //   homepage: null,
-  //   wiki: null,
-  // };
-
-  marvelService = new Service();
-
-  // первичный запрос для построения компонента
-  componentDidMount() {
-    this.updateChar();
-    //this.timedId = setInterval(this.updateChar, 10000);
-  }
-
-  // остановка интервал при ~ вызове деструктора объекта
-  componentWillUnmount() {
-    //clearInterval(this.timedId);
-  }
+  //метод для кпопки [TRY IT] / для отображения спиннера загрузки
+  const charLoading = () => {
+    setLoading(true);
+  };
 
   // dry метод для оптимизации записи персонажа в стейт
-  onCharLoaded = (char) => {
-    this.setState({ char, loading: false });
-  };
-
-  //метод для try it
-  onCharLoading = () => {
-    this.setState({
-      loading: true,
-    });
-  };
-
-  updateChar = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.onCharLoading();
-    this.marvelService
-      .getCharacter(id)
-      .then(this.onCharLoaded)
-      .catch(this.onError); // вызов метода для обработки ошибки
+  const onCharLoaded = (char) => {
+    setChar(char);
+    setLoading(false);
   };
 
   // обработка ошибки
-  onError = async () => {
-    this.setState({ loading: false, error: true });
+  const onError = () => {
+    setLoading(false);
+    setError(true);
   };
 
-  render() {
-    const { char, loading, error } = this.state;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    // нет загрузки или нет ошибки -> View, иначе -> null
-    const content = !(loading || error) ? <View char={char} /> : null;
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  // нет загрузки или нет ошибки -> View, иначе -> null
+  const content = !(loading || error) ? <View char={char} /> : null;
 
-    return (
-      <div className="randomchar">
-        {errorMessage}
-        {spinner}
-        {content}
-        <div className="randomchar__static">
-          <p className="randomchar__title">
-            Random character for today!
-            <br />
-            Do you want to get to know him better?
-          </p>
-          <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main" onClick={this.updateChar}>
-            <div className="inner">try it</div>
-          </button>
-          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
-        </div>
+  return (
+    <div className="randomchar">
+      {errorMessage}
+      {spinner}
+      {content}
+      <div className="randomchar__static">
+        <p className="randomchar__title">
+          Random character for today!
+          <br />
+          Do you want to get to know him better?
+        </p>
+        <p className="randomchar__title">Or choose another one</p>
+        <button className="button button__main" onClick={updateChar}>
+          <div className="inner">try it</div>
+        </button>
+        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 // простой рендерящий компонент
 const View = ({ char }) => {
@@ -117,3 +91,117 @@ const View = ({ char }) => {
 };
 
 export default RandomChar;
+
+
+// class RandomChar extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   // пустой объект char эквивалентен записи в null
+//   state = {
+//     char: {},
+//     loading: true,
+//     error: false,
+//   };
+
+//   // state = {
+//   //   name: null,
+//   //   description: null,
+//   //   preview: null,
+//   //   homepage: null,
+//   //   wiki: null,
+//   // };
+
+//   marvelService = new Service();
+
+//   // первичный запрос для построения компонента
+//   componentDidMount() {
+//     this.updateChar();
+//     //this.timedId = setInterval(this.updateChar, 10000);
+//   }
+
+//   // остановка интервал при ~ вызове деструктора объекта
+//   componentWillUnmount() {
+//     //clearInterval(this.timedId);
+//   }
+
+//   // dry метод для оптимизации записи персонажа в стейт
+//   onCharLoaded = (char) => {
+//     this.setState({ char, loading: false });
+//   };
+
+//   //метод для try it
+//   onCharLoading = () => {
+//     this.setState({
+//       loading: true,
+//     });
+//   };
+
+//   updateChar = () => {
+//     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+//     this.onCharLoading();
+//     this.marvelService
+//       .getCharacter(id)
+//       .then(this.onCharLoaded)
+//       .catch(this.onError); // вызов метода для обработки ошибки
+//   };
+
+//   // обработка ошибки
+//   onError = async () => {
+//     this.setState({ loading: false, error: true });
+//   };
+
+//   render() {
+//     const { char, loading, error } = this.state;
+//     const errorMessage = error ? <ErrorMessage /> : null;
+//     const spinner = loading ? <Spinner /> : null;
+//     // нет загрузки или нет ошибки -> View, иначе -> null
+//     const content = !(loading || error) ? <View char={char} /> : null;
+
+//     return (
+//       <div className="randomchar">
+//         {errorMessage}
+//         {spinner}
+//         {content}
+//         <div className="randomchar__static">
+//           <p className="randomchar__title">
+//             Random character for today!
+//             <br />
+//             Do you want to get to know him better?
+//           </p>
+//           <p className="randomchar__title">Or choose another one</p>
+//           <button className="button button__main" onClick={this.updateChar}>
+//             <div className="inner">try it</div>
+//           </button>
+//           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// // простой рендерящий компонент
+// const View = ({ char }) => {
+//   const { name, description, preview, homepage, wiki } = char;
+
+//   return (
+//     <div className="randomchar__block">
+//       <img src={preview} alt="Marvel character" className="randomchar__img" />
+//       <div className="randomchar__info">
+//         <p className="randomchar__name">{name}</p>
+//         <p className="randomchar__descr">{description}</p>
+//         <div className="randomchar__btns">
+//           <a href={homepage} className="button button__main">
+//             <div className="inner">homepage</div>
+//           </a>
+//           <a href={wiki} className="button button__secondary">
+//             <div className="inner">Wiki</div>
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RandomChar;
